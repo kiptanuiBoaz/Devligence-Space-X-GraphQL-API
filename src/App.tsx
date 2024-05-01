677b9af2c4bdb785acec34b7c864174115384d58
Kiptanui Boaz <boaserem022@gmail.com>
2024-05-01 14:41:42 +0300

    add company and launches query

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { FILMS_QUERY } from './graphql/query';
+import { Company_And_Launches_Query } from './graphql/query';
 import { useQuery } from '@apollo/client';
 
 
 function App() {
   //from graphql query
-  const { data, loading, error } = useQuery(FILMS_QUERY);
-  console.log(data, "data")
-  console.log(loading, "loading")
-  console.log(error, "error")
+  const { data, loading, error } = useQuery(Company_And_Launches_Query);
+  console.log(error, "data")
+
   if (loading) return "Loading...";
   if (error) return <pre>{error.message}</pre>
 
@@ -25,10 +24,10 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>SpaceX Launches</h1>
+      <h1>{data.company.name}</h1>
       <div className="card">
         <ul>
-          {data.launchesPast.map((launch) => (
+          {data.launches.map((launch) => (
             <li key={launch.id}>{launch.mission_name}</li>
           ))}
         </ul>
--- src/graphql/query.ts
+++ src/graphql/query.ts
@@ -1,10 +1,31 @@
 import { gql } from "@apollo/client";
 
-export const FILMS_QUERY = gql`
-  {
-    launchesPast(limit: 10) {
+export const Company_And_Launches_Query = gql`
+    {
+    company {
+      ceo
+      coo
+      cto
+      name
+      summary
+      headquarters {
+        city
+      }
+      test_sites
+      vehicles
+      launch_sites
+    }
+    launches(limit:20) {
       id
       mission_name
+      rocket {
+        rocket_name
+      }
+      launch_site {
+        site_name
+      }
+      launch_success
+      launch_date_local
     }
   }
 `;
