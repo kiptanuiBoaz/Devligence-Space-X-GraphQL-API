3f255d57fd8c2eb2eb94deaed559fd0021bd6a87
Kiptanui Boaz <boaserem022@gmail.com>
2024-05-02 08:10:20 +0300

    add page components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Company_And_Launches_Query } from './graphql/query';
 import { useQuery } from '@apollo/client';
